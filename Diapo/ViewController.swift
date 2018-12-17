3326d260f4bb5cd7047d922dcbd951b254d3226a
cesmai <cecilemaigrot@yahoo.fr>
2018-12-17 16:43:07 +0100

    Hide or show navigation stack view on touch screen

--- Diapo/ViewController.swift
+++ Diapo/ViewController.swift
@@ -10,34 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var ui_imageView: UIImageView!
+    @IBOutlet weak var ui_navStack: UIStackView!
     var photoIndex: Int = 1
     var timer: Timer?
+    var navTimer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         // Init
-        print("Init image")
-        imageView.image = UIImage(named: "photo\(photoIndex)")
+        //print("Init image")
+        ui_imageView.image = UIImage(named: "photo\(photoIndex)")
+        ui_navStack.isHidden = true
         
         // Launch diaporama
-        print("Begin diaporama")
+        //print("Begin diaporama")
         
         timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (_) in
             
             self.photoIndex += 1
             
             var photoName = "photo\(self.photoIndex)"
-            print("Display photo = " + photoName)
-            self.imageView.image = UIImage(named: photoName)
+            //print("Display photo = " + photoName)
+            self.ui_imageView.image = UIImage(named: photoName)
             if (self.photoIndex == 3) {
                 self.photoIndex = 0
             }
         }
         
-        print("End diaporama")
+        //print("End diaporama")
     }
     
     @IBAction func stopButtonTouched() {
@@ -46,6 +49,7 @@ class ViewController: UIViewController {
         
     @IBAction func prevButtonTouched() {
         timer?.invalidate()
+        showNavStack()
         
         if photoIndex > 1 {
             photoIndex -= 1
@@ -53,13 +57,14 @@ class ViewController: UIViewController {
             photoIndex = 3
         }
         var photoName = "photo\(photoIndex)"
-        print("Display photo = " + photoName)
-        imageView.image = UIImage(named: photoName)
+        //print("Display photo = " + photoName)
+        ui_imageView.image = UIImage(named: photoName)
         
     }
     
     @IBAction func nextButtonTouched() {
         timer?.invalidate()
+        showNavStack()
         
         if photoIndex < 3 {
             photoIndex += 1
@@ -67,7 +72,38 @@ class ViewController: UIViewController {
             photoIndex = 1
         }
         var photoName = "photo\(photoIndex)"
-        print("Display photo = " + photoName)
-        imageView.image = UIImage(named: photoName)
+        //print("Display photo = " + photoName)
+        ui_imageView.image = UIImage(named: photoName)
+    }
+    
+    func showNavStack() {
+        // Reset Timer
+        navTimer?.invalidate()
+        
+        // Show nav during few seconds
+        self.ui_navStack.isHidden = false
+        navTimer = Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { (_) in
+            self.ui_navStack.isHidden = true
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touchesBegan")
+        
+        print(ui_navStack.isHidden)
+        
+        if ui_navStack.isHidden {
+            showNavStack()
+        } else {
+            ui_navStack.isHidden = true
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touchesMoved")
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touchesEnded")
     }
 }
